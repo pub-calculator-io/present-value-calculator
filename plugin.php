bf907f7758f61d863218b39c09eced53a9086684
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:16:42 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_present_value_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Present Value Calculator by Calculator.iO";
 
 function display_ci_present_value_calculator(){
